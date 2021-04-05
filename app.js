5aa813057c0a5041d7e3826ca81e67f641193384
somya <somyaprasad2705@gmail.com>
2021-04-05 23:01:33 +0530

    adding new files

---
+++ app.js
@@ -0,0 +1,16 @@
+function toggle1(frm1) {
+    var con = document.getElementById("con1");
+    con.style.display = frm1.checked ? "block" : "none";
+}
+
+
+function toggle2(frm2) {
+    var con = document.getElementById("con2");
+    con.style.display = frm2.checked ? "block" : "none";
+}
+
+
+function toggle3(frm3) {
+    var con = document.getElementById("con3");
+    con.style.display = frm3.checked ? "block" : "none";
+}
